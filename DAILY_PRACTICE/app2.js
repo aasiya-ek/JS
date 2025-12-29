4115f3b75f8944726a6e935e2c27e22681f4ff76
Aasiya Ekram <ekramaasiya98@gmail.com>
2025-12-29 13:28:02 +0530

    Files Modified

--- DAILY_PRACTICE/app2.js
+++ DAILY_PRACTICE/app2.js
@@ -7,3 +7,29 @@ console.log(email);
 let arr=["neha",18,"neha@gmail.com",true];
 console.log(arr);
 
+// let arr2= new Array()
+// console.log(arr2);
+// arr2[0]="sweta"
+// arr2[1]=23
+// arr2[2]="sweta@gmail.com"
+// arr2[3]=false
+// console.log(arr2);
+
+// let arr2=new Array("rohit",23,true);
+
+// let arr3=new Array[2,3,4]
+let arr3=new Array(2)
+arr3[0]=23
+arr3[1]=34
+arr3[2]=45
+
+console.log(arr3);
+
+
+let arr4 =[
+    [2,14],
+    [3,4]
+]
+console.log(arr4[0][1]);
+
+
