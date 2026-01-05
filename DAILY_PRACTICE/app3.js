fb413c6ccfdf2679b6affcbc925320c0c3b934d7
Aasiya Ekram <ekramaasiya98@gmail.com>
2026-01-05 13:31:44 +0530

    Flies Uploaded

---
+++ DAILY_PRACTICE/app3.js
@@ -0,0 +1,42 @@
+// const arr = [1, 2, 3];
+// const clone = [...arr];
+// console.log(arr);
+// console.log(clone);
+// arr.push(3);
+// clone.push(7);
+// console.log(clone);
+// console.log(arr);
+
+
+
+// const arr=[1,2,3];
+// const clone=arr.slice();
+// console.log(clone);
+
+
+// cloning of array
+// const arr=[1,2,3];
+// const clone= Array.from(arr);
+// console.log(clone);
+// arr.push(6);
+// clone.push(9);
+// console.log(arr);
+// console.log(clone);
+
+
+const arr=[1,2,3];
+const clone=[].concat(arr);
+console.log(arr);
+console.log(clone);     //1,2,3
+arr.push(21);
+clone.push(34);
+console.log(arr);
+console.log(clone);
+
+
+
+
+
+
+
+
